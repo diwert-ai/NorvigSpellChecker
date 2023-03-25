83b1fe60f0c70dba3a35885f1c249f0495b6aa7c
diwert-ai <diwert08@gmail.com>
2023-03-26 00:05:14 +0300

    add some tests

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ def main():
     text = 'Speling Errurs IN somethink. Whutever; unusuel misteakes? Fantomas World.'
     print(text)
     print(checker.correct_text(text))
+    text = 'The show eeneashiallea struggles to asstubleash a tone, but the premise is interesting enuff to make it worth ' \
+           'sticking with.'
+    print(text)
+    print(checker.correct_text(text))
 
 
 if __name__ == '__main__':
